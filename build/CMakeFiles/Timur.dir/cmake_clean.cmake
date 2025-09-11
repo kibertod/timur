c0cbb04b327421fcc463fdfef5fb160b3d167fd4
kibertod <cyber704d@gmail.com>
2025-09-11 14:24:34 +0300

    init

---
+++ build/CMakeFiles/Timur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Timur.dir/link.d"
+  "CMakeFiles/Timur.dir/src/main.cpp.o"
+  "CMakeFiles/Timur.dir/src/main.cpp.o.d"
+  "Timur"
+  "Timur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Timur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
