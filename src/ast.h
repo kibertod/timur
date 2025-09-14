a2e0f81fdbb41509e92765aff4e3bd6909b24cf2
kibertod <cyber704d@gmail.com>
2025-09-15 02:45:49 +0300

    add abstract syntax tree declaration

---
+++ src/ast.h
@@ -0,0 +1,88 @@
+#include <memory>
+#include <optional>
+#include <string>
+#include <utility>
+#include <variant>
+#include <vector>
+
+namespace ast {
+
+    template<typename T>
+    using Ptr = std::unique_ptr<T>;
+
+    struct Identifier {
+        std::string name;
+    };
+
+    struct Expression {
+        struct MemberAccess {
+            Ptr<Expression> object;
+            Identifier member;
+        };
+
+        struct MethodCall {
+            Ptr<Expression> object;
+            Identifier method;
+            std::vector<Expression> argumens;
+        };
+
+        struct Literal {
+            enum Type { Int, Str, Bool, Real };
+
+            std::string value;
+        };
+
+        std::variant<Identifier, MemberAccess, MethodCall, Literal> value;
+    };
+
+    struct Variable {
+        Identifier name;
+        std::optional<Expression> value;
+    };
+
+    struct Statement {
+        struct If {
+            struct ElIf {
+                Expression condition;
+                std::vector<Statement> body;
+            };
+
+            Expression condition;
+            std::vector<Statement> body;
+            std::vector<ElIf> elifs;
+            std::vector<Statement> else_body;
+        };
+
+        struct Assignment {
+            Expression left;
+            Expression right;
+        };
+
+        struct While {
+            Expression condition;
+            std::vector<Statement> body;
+        };
+
+        struct Return {
+            Expression value;
+        };
+
+        std::variant<If, While, Return, Variable, Expression> value;
+    };
+
+    struct Method {
+        Identifier name;
+        std::vector<std::pair<Identifier, Identifier>> arguments;
+        std::vector<Statement> body;
+    };
+
+    struct Class {
+        Identifier name;
+        std::optional<Identifier> extends;
+        std::vector<std::variant<Variable, Method>> body;
+    };
+
+    struct Root {
+        std::vector<Class> classes;
+    };
+};
